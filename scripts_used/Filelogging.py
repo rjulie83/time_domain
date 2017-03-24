3977a1ff329ada3b2a3fb01874208ac4bcf7cf87
User <vereese@ska.ac.za>
2017-03-24 07:51:07 +0200

    improvements to Timedomain_multi

--- scripts_used/Filelogging.py
+++ scripts_used/Filelogging.py
@@ -1,27 +1,29 @@
-import sys
-
+import sys,socket,time
+import library.vna_filelogging as flog
 
 
 if __name__ == "__main__":
 	IPaddress = "10.97.64.118"
-	vna = connect(IPaddress, port=5025, sleeptime = 1)
-	initialise(vna)
+	vna = flog.connect(IPaddress, port=5025, sleeptime = 1)
+	flog.initialise(vna)
 
-	frequency_string = get_trace_string_from_vna(0,vna)
+	frequency_string = flog.get_trace_string_from_vna(0,vna)
 	text_file = open('data/frequency.txt', "w")
 	text_file.write(frequency_string)
 	text_file.close()
 	
 
-	number_of_datasets_per_file = 10
-	minutes = 60*6*6#3756;    #approximately 24 hours
-	iteration = 0;
+	number_of_datasets_per_file = 100
+	files = 1
+	iteration = 0
 	start_time = time.time()
-	for i in range(minutes):
+	for i in range(files):
     		temp_string_list = []
     		for i in range(number_of_datasets_per_file):
-        		temp_string_list.append(str(time.time())+' '+get_trace_string_from_vna(1,vna))        
-    			write_string_to_file(temp_string_list)
-    		print iteration,len(temp_string_list[0].split(',')),(time.time() - start_time)
+			sys.stdout.write('.')
+        		temp_string_list.append(str(time.time())+' '+flog.get_trace_string_from_vna(1,vna))        
+    		flog.write_string_to_file(temp_string_list)
+    		print "File $i/%i %2.0f%%  Points check:%i Hours since start: %i" % (iteration,files-1,100.0*iteration/(files-1),len(temp_string_list[0].split(',')),int(time.time() - start_time)/3600)
     		iteration += 1
 	print (time.time() - start_time)
+	vna.close()
