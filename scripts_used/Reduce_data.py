f5e1bf893ba2814a8c7e88b704540a79484b25b2
User <vereese@ska.ac.za>
2017-04-19 11:27:28 +0200

    Update to reduce date

--- scripts_used/Reduce_data.py
+++ scripts_used/Reduce_data.py
@@ -1,3 +1,4 @@
+import progress.bar as Bar
 import library.timedomain as td
 import library.fileprocessing as fp
 import numpy as np
@@ -11,16 +12,17 @@ if __name__ == '__main__':
 		sys.exit()
 		
 	timestart = time.time()
-	
 	F = fp.loadfile_frequency('frequency.txt')
 	files = glob.glob('*.txt')
 	files.sort(key=os.path.getmtime)
 	files.remove('frequency.txt')
+	bar = Bar('Processing',max = len(files))
 	iteration = 0
 	with open('data.csv','a') as data_file:
 		data_file.write('Timestamp,Delay1[m],Delay1_amplitude[dB],Delay2[m],Delay2_amplitude[dB]\n')
 	
 	for filename in files:
+		bar.next()
 		print 'File %i out of %i being processed...' % (iteration+1,len(files))
 		timestamps_temp,temp_sdata = fp.loadfile_multisweep(filename)
 		heights1 = [];heights2 = []
@@ -29,11 +31,11 @@ if __name__ == '__main__':
 			delay1,delay2,height1,height2 = td.transform(F,i,verbose=0,overs=oversampling_factor)
 			heights1.append(height1)
 			heights2.append(height2)
-			sys.stdout.write('.')
-			sys.stdout.flush()
+			#sys.stdout.write('.')
+			#sys.stdout.flush()
 			with open('data.csv','a') as data_file:
 				data_file.write('%f,%3.10f,%2.2f,%3.10f,%2.2f\n' % (timestamps_temp[n],delay1,height1,delay2,height2))
-		sys.stdout.write('\nFile process time= %0.0fs, S21_dB_average= %2.0fdB and %2.0fdB\n' % (time.time()-loop_timestart,np.mean(heights1),np.mean(heights2)))
+		#sys.stdout.write('\nFile process time= %0.0fs, S21_dB_average= %2.0fdB and %2.0fdB\n' % (time.time()-loop_timestart,np.mean(heights1),np.mean(heights2)))
 		iteration = iteration + 1
 		
 	#data_file = open('data.csv','w')
@@ -41,5 +43,5 @@ if __name__ == '__main__':
 	#for i in range(len(timestamps)):
 	#	data_file.write('%f,%3.10f,%2.2f,%3.10f,%2.2f\n' % (timestamps[i],delay1_data[i],height1_data[i],delay2_data[i],height2_data[i]))
 	#data_file.close()
-
+	bar.finish()
 	print (time.time()-timestart)
