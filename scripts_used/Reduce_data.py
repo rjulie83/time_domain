5caaa3ab8517e2244f7a299fe6a80bf4baae0a61
Roufurd Julie <roufurd.julie@gmail.com>
2017-05-21 10:48:12 +0200

    Updates to reduce data code

--- scripts_used/Reduce_data.py
+++ scripts_used/Reduce_data.py
@@ -1,4 +1,4 @@
-import progress.bar as Bar
+from progress.bar import Bar
 import library.timedomain as td
 import library.fileprocessing as fp
 import numpy as np
@@ -16,14 +16,13 @@ if __name__ == '__main__':
 	files = glob.glob('*.txt')
 	files.sort(key=os.path.getmtime)
 	files.remove('frequency.txt')
-	bar = Bar('Processing',max = len(files))
+	bar = Bar('Processing',max=len(files)*100)
 	iteration = 0
 	with open('data.csv','a') as data_file:
 		data_file.write('Timestamp,Delay1[m],Delay1_amplitude[dB],Delay2[m],Delay2_amplitude[dB]\n')
 	
 	for filename in files:
-		bar.next()
-		print 'File %i out of %i being processed...' % (iteration+1,len(files))
+		#print 'File %i out of %i being processed...' % (iteration+1,len(files))
 		timestamps_temp,temp_sdata = fp.loadfile_multisweep(filename)
 		heights1 = [];heights2 = []
 		loop_timestart = time.time()
@@ -33,6 +32,7 @@ if __name__ == '__main__':
 			heights2.append(height2)
 			#sys.stdout.write('.')
 			#sys.stdout.flush()
+			bar.next()
 			with open('data.csv','a') as data_file:
 				data_file.write('%f,%3.10f,%2.2f,%3.10f,%2.2f\n' % (timestamps_temp[n],delay1,height1,delay2,height2))
 		#sys.stdout.write('\nFile process time= %0.0fs, S21_dB_average= %2.0fdB and %2.0fdB\n' % (time.time()-loop_timestart,np.mean(heights1),np.mean(heights2)))
